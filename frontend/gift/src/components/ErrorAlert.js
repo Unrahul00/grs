aa66adb80b23a32e8ba3f0f5134625f998169e6c
Unrahul00 <unrahul1@gmail.com>
2025-05-28 13:17:54 +0530

    changed

--- frontend/gift/src/components/ErrorAlert.js
+++ frontend/gift/src/components/ErrorAlert.js
@@ -6,7 +6,7 @@ const ErrorAlert = ({ message }) => {
   return (
     <Alert variant="destructive" className="bg-red-500/10 border-red-500 text-red-400">
       <AlertCircle className="h-4 w-4" />
-      <AlertTitle>Error</AlertTitle>
+      <AlertTitle>BIG ERRROR</AlertTitle>
       <AlertDescription>{message}</AlertDescription>
     </Alert>
   );
